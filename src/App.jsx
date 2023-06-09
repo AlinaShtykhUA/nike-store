0870df6913fcb0df6828e481abcac172aa6f30d9
Alina <39155762+AlinaShtykhUA@users.noreply.github.com>
2023-06-09 14:42:29 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Hero } from "./components";
+import { heroapi } from './data/data.js'
+
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Hero heroapi={heroapi} />
+      </main>
+    </>
+  )
+}
+
+export default App
