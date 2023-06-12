07dd99dd45981a944a4ac740d7daaa064ed53c53
Alina Shtykh <alinashtykh@gmail.com>
2023-06-12 17:31:15 +0200

    FlexContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Hero, Sales } from "./components";
-import { heroapi, popularsales, toprateslaes } from './data/data.js'
+import { FlexContent, Hero, Sales } from "./components";
+import { heroapi, popularsales, toprateslaes, highlight, sneaker } from './data/data.js'
 
 
 const App = () => {
@@ -7,8 +7,14 @@ const App = () => {
     <>
       <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
+
         <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+
         <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        
+        
       </main>
     </>
   )
---
+++ src/components/FlexContent.jsx
@@ -0,0 +1,25 @@
+
+const FlexContent = ({ifExists, endpoint: {heading, title, text, btn, url, img }}) => {
+  return (
+    <>
+      <div className={`flex items-center justify-between lg:flex-col lg:justify-center nike-container ${ifExists ? 'flex-row-reverse' : 'flex-row'}`}>
+        <div className="max-w-lg lg:max-w-none w-full md:text-center">
+          <h2 className="text-4xl sm:text-3xl font-bold text-gradient">{heading}</h2>
+          <h3 className="text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-bold text-slate-900 filter drop-shadow-lg">{title}</h3>
+          <p className="xl:text-sm my-4 text-slate-900">{text}</p>
+          <a className="button-theme bg-slate-900 shadow-slate-900 text-slate-100 py-1.5" target={"_blank"} href={url} >{btn}</a>
+        </div>
+
+        <div className="flex items-center justify-center max-w-xl relative lg:max-w-none w-full">
+          <img 
+            src={img}
+            alt={`img/${heading}`}
+            className={`w-auto object-fill  transitions-theme ${ifExists ? 'h-60 lg:h56 md:h-52 sm:h-44 xsm:h-3/6 rotate-6 hover:-rotate-12' : 'h-72 lg:h-64 md:h-60 sm:h-48 xsm:h-40 rotate-[19deg] hover:rotate-12'}`}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FlexContent
